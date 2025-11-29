881600586a668131e09fcd5fde8658143c452e72
Barco Exequiel Alejandro <exebarco280320@gmail.com>
2025-11-29 04:49:15 +0000

    add page experiencfe and projects

---
+++ app/(public)/experience/page.tsx
@@ -0,0 +1,13 @@
+import { Container } from "@/components/layout/container";
+import AboutSection from "@/components/sections/public/about-section";
+import { PageSection } from "@/components/layout/section";
+
+export default function ExperiencePage() {
+  return (
+    <Container>
+      <PageSection>
+        Experience
+      </PageSection>
+    </Container>
+  );
+}
---
+++ app/(public)/projects/page.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@/components/layout/container";
+import { PageSection } from "@/components/layout/section";
+
+export default function ProjectsPage() {
+  return (
+    <Container>
+      <PageSection>
+        Projects
+      </PageSection>
+    </Container>
+  );
+}
