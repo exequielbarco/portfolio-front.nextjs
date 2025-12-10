268098d06662d79d0d9bddd3e9c4b836ad057e67
ebarco <ebarco@bse.com.ar>
2025-12-10 10:38:29 -0300

    about-section de mas eliminado

--- app/(public)/experience/page.tsx
+++ app/(public)/experience/page.tsx
@@ -1,13 +1,10 @@
 import { Container } from "@/components/layout/container";
-import AboutSection from "@/components/sections/public/about-section";
 import { PageSection } from "@/components/layout/section";
 
 export default function ExperiencePage() {
   return (
     <Container>
-      <PageSection>
-        Experience
-      </PageSection>
+      <PageSection>Experience</PageSection>
     </Container>
   );
 }
